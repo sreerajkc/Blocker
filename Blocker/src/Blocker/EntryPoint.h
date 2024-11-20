47945c675cace91ca8b053383215f603bf993513
sreerajkoco <koco.sreeraj@gmail.com>
2024-11-21 01:24:41 +0530

    added logger

--- Blocker/src/Blocker/EntryPoint.h
+++ Blocker/src/Blocker/EntryPoint.h
@@ -5,6 +5,9 @@ extern Blocker::Application* Blocker::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Blocker::Log::Init();
+	Blocker::Log::GetClientLogger()->warn("Initialized Log");
+
 	auto app = Blocker::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Blocker/src/Blocker/Log.cpp
@@ -0,0 +1,19 @@
+#include "Log.h"
+
+namespace Blocker 
+{
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");// T-timestamp, n - name of the logger, v-message
+		s_CoreLogger = spdlog::stdout_color_mt("BLOCKER");
+		s_CoreLogger->set_level(spdlog::level::trace);
+
+		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger->set_level(spdlog::level::trace);
+
+	}
+
+}
